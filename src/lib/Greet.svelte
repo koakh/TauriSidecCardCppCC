5670a82f2a9a10f38a05d2b2762adf4905060d7b
Mário Monteiro <marioammonteiro@gmail.com>
2023-10-12 23:17:17 +0100

    first commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { Command } from "@tauri-apps/api/shell";
+
+  let name = "";
+  let greetMsg = "";
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+
+  async function runSIdeCard() {
+    // alternatively, use `window.__TAURI__.shell.Command`
+    // `binaries/my-sidecar` is the EXACT value specified on `tauri.conf.json > tauri > bundle > externalBin`
+    const command = Command.sidecar("binaries/app");
+    const output = await command.execute();
+    console.log(`output: [${JSON.stringify(output, undefined, 2)}]`);
+  }
+</script>
+
+<div>
+  <div class="row">
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button on:click={greet}> Greet </button>
+
+    <button on:click={runSIdeCard}> SideCard </button>
+  </div>
+  <p>{greetMsg}</p>
+</div>
